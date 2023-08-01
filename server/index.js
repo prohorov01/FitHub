044cdfde7448d0522ef3b517ac23becb64121c20
tolik00761666 <tolik007616@gmail.com>
2023-08-01 18:54:29 +0300

    start server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const path = require("path");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+const indexPath = path.resolve(__dirname, "../client/src/index.html");
+
+app.use(express.static(path.resolve(__dirname, "../client/src")));
+
+app.get("/", (req, res) => {
+  fs.readFile(indexPath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Ошибка при чтении файла:", err);
+      res.status(500).json({ error: "Ошибка сервера" });
+    } else {
+      res.send(data); 
+    }
+  });
+});
 
 app.post("/save-data", (req, res) => {
   const data = req.body.fitnessData;
